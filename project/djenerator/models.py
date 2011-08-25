bdb6dd64bc7b92a3fa9edc9d558ecd3f4d1faea1
Mostafa Mahmoud <mostafa.mahmoud@g-beehive.com>
2011-08-25 10:07:53 +0200

    Created a new project, edited the database settings, created djenerator app, added the app to the installed apps.

---
+++ project/djenerator/models.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from django.db import models
+# Create your models here.
+
+
---
+++ project/djenerator/tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+
+Replace this with more appropriate tests for your application.
+"""
+from django.test import TestCase
+
