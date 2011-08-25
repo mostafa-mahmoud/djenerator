d6433001f3660c9c4506fe5e1f62c0a52edd02f7
Mostafa Mahmoud <mostafa.mahmoud@g-beehive.com>
2011-08-25 11:54:50 +0200

    Test Cases for is instance of Model function

--- project/djenerator/tests.py
+++ project/djenerator/tests.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+This module contains tests for djenerator app.
 """
 from django.test import TestCase
+from model_reader import is_instance_of_model
+from models import ExtendingModel
+from models import NotExtendingModel
+from models import TestModel0
+from models import TestModel1
+from models import TestModelA
+from models import TestModelB
+from models import TestModelC
+from models import TestModelD
+from models import TestModelE
+from models import TestModelX
+from models import TestModelY
+
+
+class TestInstanceOfModel(TestCase):
+    def test(self):
+        models = [TestModel0, TestModel1, TestModelA, TestModelB, TestModelC,
+                 TestModelD, TestModelE, TestModelX, TestModelY, ExtendingModel]
+        for model in models:
+            self.assertTrue(is_instance_of_model(model))
+        self.assertFalse(is_instance_of_model(NotExtendingModel))
+        def not_extending_model_function():
+            pass
+        
+        self.assertFalse(is_instance_of_model(not_extending_model_function))
+
+
 
