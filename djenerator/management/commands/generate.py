d1698f8fcc7fb9a1e109c434e2bc31c74b62c0f2
Mostafa Mahmoud <mostafa.amin93@gmail.com>
2016-10-16 02:55:15 +0200

    Updated the generate command to fit the format add_argument after Django 1.8

--- djenerator/management/commands/generate.py
+++ djenerator/management/commands/generate.py
@@ -5,13 +5,19 @@ from django.core.management.base import CommandError
 
 
 class Command(BaseCommand):
-    args = '<size app_name out_name>'
-    help = 'Generates random test data'
+    args = '<size app_name output_name>'
+    help = '''Generates random test data for an app using djenerator. The
+              arguments of the command are `size app_name output_name` '''
+
+    def add_arguments(self, parser):
+        parser.add_argument('size')
+        parser.add_argument('app_name')
+        parser.add_argument('output_name')
 
     def handle(self, *args, **options):
-        size = int(args[0])
-        app_name = args[1]
-        out_put_name = args[2]
-        output_file = open(args[2] + '.json', 'w')
+        size = int(options['size'])
+        app_name = options['app_name']
+        output_name = options['output_name']
+        output_file = open(output_name + '.json', 'w')
         djenerator(app_name, size, output_file)
         output_file.close()
